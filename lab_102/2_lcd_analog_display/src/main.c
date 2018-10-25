f3c0ec8bcdf3de3068063d3c9e040b89d64a5658
Indy336 <paflazer@yahoo.com>
2018-10-25 14:52:38 +0100

    lab2Task2

--- lab_102/2_lcd_analog_display/src/main.c
+++ lab_102/2_lcd_analog_display/src/main.c
@@ -15,10 +15,23 @@
 #include "pinmappings.h"
 #include "clock.h"
 #include "stm32746g_discovery_lcd.h"
+#include <stdio.h>
+#include <adc.h>
 
 // LCD DEFINES
+// define a message boarder (note the lcd is 28 characters wide using Font24)
+#define BOARDER     "****************************"
 
-// CODE
+// specify a welcome message
+const char * welcome_message[2] = 
+{
+  "*    Ioannis Anastasiou    *",
+  "*      Welcome to SHU      *"
+};
+
+
+// map the potentiometer to GPIO PA0
+gpio_pin_t pot= {PA_0, GPIOA, GPIO_PIN_0};
 
 // this is the main method
 int main()
@@ -27,5 +40,49 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
+	
+	// initialise the potentiometer
+	init_adc(pot);
+	  
+  // initialise the lcd
+  BSP_LCD_Init();
+  BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, SDRAM_DEVICE_ADDR);
+  BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
 
+  // set the background colour to blue and clear the lcd
+  BSP_LCD_SetBackColor(LCD_COLOR_BLUE);
+  BSP_LCD_Clear(LCD_COLOR_BLUE);
+  
+  // set the font to use
+  BSP_LCD_SetFont(&Font24); 
+  
+  // print the welcome message ...
+  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+  BSP_LCD_DisplayStringAtLine(0, (uint8_t *)BOARDER);
+  BSP_LCD_DisplayStringAtLine(1, (uint8_t *)welcome_message[0]);
+  BSP_LCD_DisplayStringAtLine(2, (uint8_t *)welcome_message[1]);
+  BSP_LCD_DisplayStringAtLine(3, (uint8_t *)BOARDER); 
+    
+  // delay a little ...
+  HAL_Delay(5000);
+  while (1)
+	{
+		
+	// read the potentiometer and echo that value to the terminal
+	uint16_t adc_val = read_adc(pot);	
+	HAL_Delay (adc_val);
+
+	
+
+	// format a string based around the adc value and print to lcd
+	char str[12];
+	sprintf(str, "ADC = %4d", adc_val);
+	BSP_LCD_DisplayStringAtLine(6, (uint8_t *)str);
+
+// print the message to the lcd
+    BSP_LCD_ClearStringLine(6);
+    BSP_LCD_DisplayStringAtLine(6, (uint8_t *)str);
+    
+    HAL_Delay(100);
+}
 }
